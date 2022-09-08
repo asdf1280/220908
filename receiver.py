1027ba257f5cebc0c90b7646a2e6819ed50a5960
asdf1280 <10652364+asdf1280@users.noreply.github.com>
2022-09-08 14:47:50 +0900

    test

---
+++ receiver.py
@@ -0,0 +1,22 @@
+import socket
+
+s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s1.bind(('', 12345))
+
+while True:
+    data, addr = s1.recvfrom(2048)
+    
+    readOffset = 0
+    nameLen = int.from_bytes(data[readOffset:readOffset+4], byteorder='big')
+    readOffset += 4
+
+    name = data[readOffset:readOffset+nameLen].decode("utf-8")
+    readOffset += nameLen
+
+    messageLen = int.from_bytes(data[readOffset:readOffset+4], byteorder='big')
+    readOffset += 4
+
+    message = data[readOffset:readOffset+messageLen].decode("utf-8")
+    readOffset += messageLen
+
+    print(f"<{name}> {message}")
---
+++ sender.py
@@ -0,0 +1,19 @@
+# broadcast "hello world" to udp port 12345
+
+import socket
+
+# ask name to console
+name = input("Enter your name: ")
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+while True:
+    message = input("> ")
+    if message == "exit":
+        break
+
+    # send message to server
+    ba = bytearray()
+    ba.extend(len(name).to_bytes(4, byteorder='big'), name.encode("utf-8"))
+    ba.extend(len(message).to_bytes(4, byteorder='big'), message.encode("utf-8"))
